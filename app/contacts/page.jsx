623d76f399dd1f731a39f62c8e51ede5669aaab9
Angel1980 <58862906+Angel04172002@users.noreply.github.com>
2024-10-10 16:10:27 -0400

    Setup Next.js and add basic architecture

---
+++ app/contacts/page.jsx
@@ -0,0 +1,3 @@
+export default function Contacts() {
+    return <h1>This is Contacts Page!</h1>;
+  }
---
+++ app/resources/page.jsx
@@ -0,0 +1,30 @@
+import './Resources.css';
+
+export default function Resources() {
+
+    return (
+
+        <div className='container mt-5'>
+
+            <div className='row'>
+
+                <div className='col-md-6'>
+                    <h1 className='display-4 main-title-resources'>Качество на въздуха</h1>
+                    <p className="display-6 paragraph-resources">Дишането на здравословен въздух е жизненоважно за вашето здраве и благополучие. Научете как да следите замърсителите в дома си и да намалите риска от излагане.</p>
+
+                </div>
+
+                <div className='col-md-6'>
+
+                    <img className='animate-image-resources' width={'50%'} src="https://cdn.britannica.com/14/22314-050-04B2DD0D/element-radon-symbol-square-Rn-properties-some.jpg" alt="Image" />
+
+
+                </div>
+
+            </div>
+
+
+        </div>
+    )
+
+}
